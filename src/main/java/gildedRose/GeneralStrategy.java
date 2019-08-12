9f08f7d5d139d9c01061caa392da1d128b2735e1
xuexi666666 <235400554@qq.com>
2019-08-12 22:51:47 +0800

    对General的行为进行抽取

--- src/main/java/gildedRose/GeneralStrategy.java
+++ src/main/java/gildedRose/GeneralStrategy.java
@@ -3,6 +3,14 @@ package gildedRose;
 public class GeneralStrategy implements ItemStrategy {
     @Override
     public void update(Item item) {
-
+        if (items[i].quality > 0) {
+                items[i].quality = items[i].quality - 1;
+        }
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].sellIn < 0) {
+            if (items[i].quality > 0) {
+                items[i].quality = items[i].quality - 1;
+            }
+        }
     }
 }

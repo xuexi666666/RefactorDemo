ccf7b07a0adfd0180c87eb5166f216d16482be70
xuexi666666 <235400554@qq.com>
2019-08-12 22:58:28 +0800

    往Item类中定义一个ItemStrategy类型,并定义一个updateQuality方法,方便不同策略实例操作Item

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -1,12 +1,10 @@
 package gildedRose;
 
 public class Item {
-
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
+    private ItemStrategy itemStrategy;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,6 +12,9 @@ public class Item {
         this.quality = quality;
     }
 
+    public void updateQuality(){
+        itemStrategy.update(this);
+    }
     public String getName() {
         return name;
     }
--- src/main/java/gildedRose/ItemStrategyFactory.java
+++ src/main/java/gildedRose/ItemStrategyFactory.java
@@ -1,4 +1,5 @@
 package gildedRose;
 
 public class ItemStrategyFactory {
+
 }

374e49124d51c45487354d5967fafe76936e4350
xuexi666666 <235400554@qq.com>
2019-08-12 22:43:34 +0800

    对Backstage的行为抽取

--- src/main/java/gildedRose/BackStagePassesStrategy.java
+++ src/main/java/gildedRose/BackStagePassesStrategy.java
@@ -3,6 +3,25 @@ package gildedRose;
 public class BackStagePassesStrategy implements ItemStrategy {
     @Override
     public void update(Item item) {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].sellIn < 11) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
 
+                if (items[i].sellIn < 6) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+        items[i].sellIn = items[i].sellIn - 1;
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality;
+        }
     }
 }

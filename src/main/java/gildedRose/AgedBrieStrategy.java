0625b890be17849bea891c9c121bfd5ef947b71c
xuexi666666 <235400554@qq.com>
2019-08-12 22:41:33 +0800

    根据抽取的代码行为,通过Item对AgedBrie策略中的update进行修改

--- src/main/java/gildedRose/AgedBrieStrategy.java
+++ src/main/java/gildedRose/AgedBrieStrategy.java
@@ -3,13 +3,13 @@ package gildedRose;
 public class AgedBrieStrategy implements ItemStrategy {
     @Override
     public void update(Item item) {
-        if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality()+1);
         }
-        items[i].sellIn = items[i].sellIn - 1;
-        if (items[i].sellIn < 0) {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+        item.setSellIn(item.getSellIn()-1);
+        if (item.getSellIn() < 0) {
+            if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality()+1);
             }
         }
     }
